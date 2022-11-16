10055a074459b1cf0d1c91690bd3b73f30f92090
InaamR <83518345+InaamR@users.noreply.github.com>
2022-11-16 16:37:50 +0100

    fetch

--- process/fetch.php
+++ process/fetch.php
@@ -12,23 +12,21 @@ if(isset($_POST["post_id"]) || isset($_GET['post_id_json']))
 	$PDO_query->execute();
 	$output = '';
 	$modale = $PDO_query->fetch();
+    $PDO_query->closeCursor();
 
     $response = [
         "name"  => $modale['eg_produit_nom'],
         "prix"  => $modale['eg_produit_prix'],
     ];
 
-    // Get product images
     $PDO_query_produit_img_modale = Bdd::connectBdd()->prepare("SELECT * FROM eg_image_produit WHERE eg_image_produit_statut = 1 AND eg_produit_id = :eg_produit_id ORDER BY eg_image_produit_ordre DESC LIMIT 3");
     $PDO_query_produit_img_modale->bindParam(":eg_produit_id", $modale["eg_produit_id"], PDO::PARAM_INT);
     $PDO_query_produit_img_modale->execute();
     while ($produit_image = $PDO_query_produit_img_modale->fetch()){
         $response['images'][] = "https://betatest.expert-gaming.tn" . $produit_image['eg_image_produit_nom'];
     }
-    
+    $PDO_query_produit_img_modale->closeCursor();
     echo json_encode($response);
-    
-
 }
 
 ?>
